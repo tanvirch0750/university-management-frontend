5762918c7dacf43ee6718d6cf9f3ecc88fb793f5
tanvirch0750 <tanvir0750@yahoo.com>
2023-09-25 18:47:29 +0600

    customize group wise dashboard layout

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 function NotFoundPage() {
   return (
     <div>
---
+++ src/components/ui/Contents.tsx
@@ -0,0 +1,20 @@
+'use client';
+import { Tchildren } from '@/types/global';
+import { Layout } from 'antd';
+
+const { Content } = Layout;
+
+function Contents({ children }: Tchildren) {
+  return (
+    <Content
+      style={{
+        minHeight: '100vh',
+        color: 'black',
+      }}
+    >
+      {children}
+    </Content>
+  );
+}
+
+export default Contents;
